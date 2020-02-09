fc3a19c13ea5b0596f75dd85edef9ba40f0281e0
Aprilhjy <n10074520@qut.edu.au>
2020-02-09 15:16:45 +1000

    Checkbox by value

--- SettingupEnvironment/SettingupEnvironment/Entrypoint.cs
+++ SettingupEnvironment/SettingupEnvironment/Entrypoint.cs
@@ -12,20 +12,19 @@ class Entrypoint
     static void Main()
     {
         string url = "http://testing.todvachev.com/special-elements/check-button-test-3/";
-        string option = "3";
+        string option = "1";
      
         driver.Navigate().GoToUrl(url);
 
         checkBox = driver.FindElement(By.CssSelector("#post-33 > div > p:nth-child(8) > input[type=checkbox]:nth-child("+ option +")"));
 
-        if (checkBox.GetAttribute("checked") == "true")
-        {
-            Console.WriteLine("This checkbox is checked!");
-        }
-        else
-        {
-            Console.WriteLine("This checkbox is NOT checked");
-        }
+        Console.WriteLine(checkBox.GetAttribute("value"));
+
+        option = "3";
+
+        checkBox = driver.FindElement(By.CssSelector("#post-33 > div > p:nth-child(8) > input[type=checkbox]:nth-child(" + option + ")"));
+
+        Console.WriteLine(checkBox.GetAttribute("value"));
 
         driver.Quit();
 
