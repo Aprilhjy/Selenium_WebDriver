01dffa4723164315568a0aca9e41b907ebeab3f2
April He <a.he@josari.io>
2020-02-10 19:52:44 +1000

    Page Object Model

---
+++ SettingupEnvironment/AutoFramework/UIElements/HomePage.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace AutoFramework
+{
+    public class HomePage
+    {
+        [System.Obsolete]
+        public HomePage()
+        {
+            PageFactory.InitElements(Driver.driver, this);
+        }
+
+        [FindsBy(How = How.CssSelector, Using = "#page-17 > header > h1")]
+        public IWebElement Headline { get; set; }
+    }
+}
