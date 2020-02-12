6c9835f790bdfdadac1ad71d600656d449ed998b
Aprilhjy <n10074520@qut.edu.au>
2020-02-12 14:36:27 +1000

    Screenshot

--- SettingupEnvironment/AutoFramework/EntryPoint.cs
+++ SettingupEnvironment/AutoFramework/EntryPoint.cs
@@ -1,9 +1,30 @@
-﻿namespace AutoFramework
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+
+namespace AutoFramework
 {
     public class EntryPoint
     {
-        public static void Main()
+        static void Main()
         {
+            IWebDriver chrome = new ChromeDriver();
+
+            string screenshotsDirectory = Directory.GetCurrentDirectory() + @"\screenshots";
+
+            chrome.Navigate().GoToUrl("http://google.com");
+
+            Screenshot googleScreenshot = ((ITakesScreenshot)chrome).GetScreenshot();
+
+            if (Directory.Exists(screenshotsDirectory))
+            {
+                Directory.CreateDirectory(Directory.GetCurrentDirectory() + @"\screenshots");
+            }
+
+         
+            googleScreenshot.SaveAsFile(Directory.GetCurrentDirectory() + @"\screenshots\googlescreenshot.png", ScreenshotImageFormat.Png);
+
+            chrome.Quit();
         }
     }
 }
